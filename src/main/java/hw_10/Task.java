1091b896b8d42288bef53143528c183dfd30ac7f
yaworskiy <yaworskiy23@mail.ru>
2026-06-23 11:41:01 +0300

    доработал класс task

--- src/main/java/hw_10/Task.java
+++ src/main/java/hw_10/Task.java
@@ -1,4 +1,32 @@
 package hw_10;
 
 public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    // Пометить задачу как выполненную
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    // Получить статус - выполнена задача или нет
+    public boolean isDone() {
+        return isDone;
+    }
+
+    // Получить название задачи
+    public String getName() {
+        return name;
+    }
+
+    // Вывести информацию о задаче
+    public void printInfo() {
+        String status = isDone ? "[x]" : "[ ]";
+        System.out.println(status + " " + name);
+    }
 }
